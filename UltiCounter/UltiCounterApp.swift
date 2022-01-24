360326b481b4e01fc75c0ae09cf3d78f0b315319
Jonathan Goldsmith <jonathangoldsmith214@gmail.com>
2022-01-23 16:35:55 -0800

    Initial Commit

---
+++ UltiCounter/UltiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltiCounterApp.swift
+//  UltiCounter
+//
+//  Created by Jonathan Goldsmith on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct UltiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
